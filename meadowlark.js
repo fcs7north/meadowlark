12590b3683643963f0249e275958b4481fa0ca72
KJHong <fcs7north@gmail.com>
2021-12-03 17:05:02 +0900

    진도-21.12.03자 업데이트

--- meadowlark.js
+++ meadowlark.js
@@ -4,7 +4,16 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('handlebars', engine())
+app.engine('handlebars', engine({
+	defaultLayout: 'main',
+	helpers: {
+		section: function(name, options) {
+			if(!this._sections) this._sections = {}
+			this._sections[name] = options.fn(this)
+			return null
+		},
+	},
+}))
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
@@ -13,6 +22,8 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
+
+app.get('/section-test', handlers.sectionTest)
 
 // custom 404 page
 app.use(handlers.notFound)
